4306386c1f378d5dd7ba525ee50f78f2d8e54d93
orapanpp <oorapan.7485@gmail.com>
2022-07-10 20:35:57 +0700

    Code Midterm V1.0 : Create Project By orapan phonjan

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_6319c10023/views/login_ui.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login_UI(),
+      theme: ThemeData(
+        fontFamily: 'Kanit',
+      ),
+    ),
+  );
+}
---
+++ lib/views/login_ui.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_6319c10023/views/register_ui.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Login_UI extends StatefulWidget {
+  const Login_UI({Key? key}) : super(key: key);
+
+  @override
+  State<Login_UI> createState() => _Login_UIState();
+}
+
+class _Login_UIState extends State<Login_UI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:  Color.fromARGB(255, 255, 255, 255),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 130.0,
+                width: 200.0,
+                child: Image.asset(
+                  'assets/images/flutter_logo.png',
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                'Welcome to FLUTTER',
+                style: TextStyle(
+                  color: Colors.grey[800],
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'DESIGN YOUR LIFE',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20.0,
+                ),
+              ),
+              Text(
+                'DESIGN YOUR FUTURE',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20.0,
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Container(
+                width: 300.0,
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(FontAwesomeIcons.user),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'ป้อนรหัสนักศึกษา',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Container(
+                width: 300.0,
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(FontAwesomeIcons.lock),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'ป้อนรหัสผ่าน',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              GestureDetector(
+                child: Text(
+                  textAlign: TextAlign.right,
+                  'Forgot Password?',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15.0,
+                  ),
+                ),
+                onTap: () {},
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              SizedBox(
+                width: 200.0,
+                height: 50.0,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'LOG IN',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 0, 46, 126),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        50.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                'Or login with',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+              ListTile(
+                title: Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        width: 100,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Icon(FontAwesomeIcons.facebook),
+                              Text(
+                                "  Facebook",
+                              ),
+                            ],
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromARGB(255, 34, 107, 233),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                8,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        width: 100,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Icon(FontAwesomeIcons.google),
+                              Text(
+                                "  Google",
+                              ),
+                            ],
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromARGB(255, 255, 0, 0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                8,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an accound?',
+                    ),
+                    GestureDetector(
+                      child: Text(
+                        'Sign Up',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RegisterUI(),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'Create By 6319C10023',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+}
